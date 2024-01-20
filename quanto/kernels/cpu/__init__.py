83d716a4b1a6989b8ee4fa71d09ab3b0c3ae72cf
David Corvoysier <david@huggingface.co>
2024-01-20 16:28:00 +0100

    feat(kernels): optimize cpu_lib for speed

--- quanto/kernels/cpu/__init__.py
+++ quanto/kernels/cpu/__init__.py
@@ -4,4 +4,4 @@ from torch.utils.cpp_extension import load
 
 
 module_path = os.path.dirname(__file__)
-cpu_lib = load(name="quanto_cpu", sources=[f"{module_path}/unpack.cpp"])
+cpu_lib = load(name="quanto_cpu", sources=[f"{module_path}/unpack.cpp"], extra_cflags=["-O3"])
